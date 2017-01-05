fa7c03c36052b6ed05e53457835b25b5c5f7831d
Alex Yue <alex@querybase.com>
2017-01-05 15:42:16 -0500

    Install script update

--- install.sh
+++ install.sh
@@ -100,7 +100,7 @@ pip install -r ../middlebox/requirements.txt
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-
+cd ../web && npm install
 
 
 # update-rc.d mongod enable
